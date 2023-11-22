331a7d9d323832ee624176ad5196ea970a192ca3
Yassa122 <yassa.ashraf@yahoo.com>
2023-11-22 14:25:54 +0200

    user models added

--- backend/Models/usersModelSchema.js
+++ backend/Models/usersModelSchema.js
@@ -1,25 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true
   },
   role: {
     type: [String],
-    required: true,
+    enum: ['user', 'admin', 'supportAgent', 'manager'],
+    required: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
