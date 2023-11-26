7f140c109cccc2687ee3e380e416952a2dc400b8
Yassa122 <yassa.ashraf@yahoo.com>
2023-11-26 18:43:42 +0200

    added user routes

---
+++ backend/Routes/usersRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../Controller/userController'); // Adjust the path to where your UserController is located
+
+router.post('/', UserController.createUser);
+router.get('/', UserController.getAllUsers);
+router.get('/:id', UserController.getUserById);
+router.put('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
+
+module.exports = router;
