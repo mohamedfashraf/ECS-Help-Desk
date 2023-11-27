a56d00a87ea314f90ab7d97f8ae11c5428a65442
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-11-27 19:37:09 +0200

    create chatMessages
    create chatMessages now works

--- backend/Controller/chatMessagesController.js
+++ backend/Controller/chatMessagesController.js
@@ -1,10 +1,17 @@
-const chatMessages = require('../Models/chatMessagesModelSchema');
+const mongoose = require('mongoose');
+const ChatMessages = require('../Models/chatMessagesModelSchema');
 
 // Create a new conversation
 async function createConversation(req, res) {
     try {
         const { ticketId, participants, messages } = req.body;
-        const conversation = new chatMessages({ ticketId, participants, messages });
+
+        const conversation = new ChatMessages({
+            ticketId,
+            participants,
+            messages
+        });
+
         await conversation.save();
         res.status(201).json(conversation);
     } catch (error) {
@@ -15,7 +22,7 @@ async function createConversation(req, res) {
 // Get all conversations
 async function getAllConversations(req, res) {
     try {
-        const conversations = await chatMessages.find({});
+        const conversations = await ChatMessages.find({});
         console.log('Retrieved Conversations:', conversations);
         res.status(200).json(conversations);
     } catch (error) {
@@ -26,7 +33,7 @@ async function getAllConversations(req, res) {
 // Get a specific conversation by ID
 async function getConversationById(req, res) {
     try {
-        const conversation = await chatMessages.findById(req.params.id);
+        const conversation = await ChatMessages.findById(req.params.id);
         if (!conversation) {
             return res.status(404).json({ error: 'Conversation not found' });
         }
@@ -39,13 +46,11 @@ async function getConversationById(req, res) {
 // Update a conversation by ID
 async function updateConversation(req, res) {
     try {
-        const updates = Object.keys(req.body);
-        const conversation = await chatMessages.findById(req.params.id);
+        const updates = req.body;
+        const conversation = await ChatMessages.findByIdAndUpdate(req.params.id, updates, { new: true });
         if (!conversation) {
             return res.status(404).json({ error: 'Conversation not found' });
         }
-        updates.forEach((update) => (conversation[update] = req.body[update]));
-        await conversation.save();
         res.status(200).json(conversation);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -55,7 +60,7 @@ async function updateConversation(req, res) {
 // Delete a conversation by ID
 async function deleteConversation(req, res) {
     try {
-        const conversation = await chatMessages.findByIdAndDelete(req.params.id);
+        const conversation = await ChatMessages.findByIdAndDelete(req.params.id);
         if (!conversation) {
             return res.status(404).json({ error: 'Conversation not found' });
         }
@@ -66,5 +71,9 @@ async function deleteConversation(req, res) {
 }
 
 module.exports = {
-    createConversation, getAllConversations, getConversationById, updateConversation, deleteConversation
+    createConversation,
+    getAllConversations,
+    getConversationById,
+    updateConversation,
+    deleteConversation
 };
