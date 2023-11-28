1fb8f32c6b18abc4fa844e26771cc8990d05e294
Yassa122 <yassa.ashraf@yahoo.com>
2023-11-28 15:42:45 +0200

    changed app.js

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,27 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const userRoutes = require("./Routes/usersRoute");
-const chatMessagesRoutes = require('./Routes/chatMessagesRoute');
-const securitySettingsRoutes = require('./Routes/securitySettingsRoute');
-const knowledgeBaseRoutes = require('./Routes/knowledgeBaseRoute');
-const reportsAndAnalyticsRoutes = require('./Routes/reportsAndAnalyticsRoute');
-const supportAgentRoutes = require('./Routes/supportAgentRoute');
-// JWT Token 
+const cookieParser = require("cookie-parser");
+const auth = require("./Routes/auth");
+const authenticationMiddleware = require("./Middleware/authentication");
+const ticketsRoute = require("./Routes/ticketsRoute");
+const chatMessagesRoutes = require("./Routes/chatMessagesRoute");
+const securitySettingsRoutes = require("./Routes/securitySettingsRoute");
+const knowledgeBaseRoutes = require("./Routes/knowledgeBaseRoute");
+const reportsAndAnalyticsRoutes = require("./Routes/reportsAndAnalyticsRoute");
+const supportAgentRoutes = require("./Routes/supportAgentRoute");
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // MongoDB Connection
-const mongoURI = 'mongodb://127.0.0.1:27017/SE-Project';
-mongoose.connect(mongoURI)
-
+const mongoURI = "mongodb://127.0.0.1:27017/Se_project";
+mongoose
+  .connect(mongoURI)
 
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
-
-// Middlewares
 app.use(express.json());
 
 // Routes
@@ -34,6 +39,12 @@ app.use("/api", knowledgeBaseRoutes);
 app.use("/api", reportsAndAnalyticsRoutes);
 
 app.use("/api", supportAgentRoutes);
+app.use("/api/v1", auth);
+app.use(authenticationMiddleware);
+
+app.use("/api/v1/tickets", ticketsRoute);
+
+app.use("/api/v1/users", userRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
