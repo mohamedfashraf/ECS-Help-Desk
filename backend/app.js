160b67fed98ed19c0097686375b305750081e36d
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-11-28 15:11:30 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const userRoutes = require("./Routes/usersRoute"); 
+const userRoutes = require("./Routes/usersRoute");
+const chatMessagesRoutes = require('./Routes/chatMessagesRoute');
+const securitySettingsRoutes = require('./Routes/securitySettingsRoute');
+const knowledgeBaseRoutes = require('./Routes/knowledgeBaseRoute');
+const reportsAndAnalyticsRoutes = require('./Routes/reportsAndAnalyticsRoute');
+const supportAgentRoutes = require('./Routes/supportAgentRoute');
 // JWT Token 
 
 // MongoDB Connection
-const mongoURI = 'mongodb://127.0.0.1:27017/Se_project';
+const mongoURI = 'mongodb://127.0.0.1:27017/SE-Project';
 mongoose.connect(mongoURI)
 
 
@@ -14,11 +19,21 @@ mongoose.connect(mongoURI)
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 // Middlewares
-app.use(express.json()); 
+app.use(express.json());
 
 // Routes
 
 app.use("/api/users", userRoutes);
+
+app.use("/api/chatMessages", chatMessagesRoutes);
+
+app.use("/api", securitySettingsRoutes);
+
+app.use("/api", knowledgeBaseRoutes);
+
+app.use("/api", reportsAndAnalyticsRoutes);
+
+app.use("/api", supportAgentRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
