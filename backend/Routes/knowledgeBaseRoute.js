0832a6e78bf5a7987bcb46f0d9cb74d2d15e4713
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-02 15:05:39 +0200

    update in knowledgeBase

--- backend/Routes/knowledgeBaseRoute.js
+++ backend/Routes/knowledgeBaseRoute.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const knowledgeBaseController = require('../Controller/knowledgeBaseController');
-const authorizationMiddleware = require("../Middleware/authorization"); //authorizationMiddleware(["user"])
+const authorizationMiddleware = require("../Middleware/authorization");
 
+// Create a new issue
 router.post('/', authorizationMiddleware(["admin"])
     , knowledgeBaseController.createIssue);
 
-router.get('/', authorizationMiddleware(["user"])
-    , knowledgeBaseController.getAllIssues);
+// Get all issues or search based on keyword
+router.get('/', authorizationMiddleware(["user", "admin"])
+    , knowledgeBaseController.getAllOrSearchIssues);
 
-router.get('/:id', authorizationMiddleware(["user"])
+// Get all issues grouped by category
+router.get('/categories', authorizationMiddleware(["user", "admin"])
+    , knowledgeBaseController.getAllIssuesByCategory);
+
+// Get an issue by ID
+router.get('/:id', authorizationMiddleware(["user", "admin"])
     , knowledgeBaseController.getIssueById);
 
+// Update an issue by ID
 router.put('/:id', authorizationMiddleware(["admin"])
     , knowledgeBaseController.updateIssue);
 
+// Delete an issue by ID
 router.delete('/:id', authorizationMiddleware(["admin"])
     , knowledgeBaseController.deleteIssue);
 
