87cc93a11d55f010439243f2cc84b19e63798d21
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-04 01:42:46 +0200

    added live chat
    working on server but still not save in database

---
+++ backend/Models/liveChatSchema.js
@@ -0,0 +1,30 @@
+// ChatMessage.js
+const mongoose = require('mongoose');
+
+const liveChatSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User' // Assuming sender is a User
+    },
+    agentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: false
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const ChatMessage = mongoose.model('ChatMessage', liveChatSchema);
+
+module.exports = ChatMessage;
--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+// Import required packages and modules
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -14,43 +15,59 @@ const reportsAndAnalyticsRoutes = require("./Routes/reportsAndAnalyticsRoute");
 const supportAgentRoutes = require("./Routes/supportAgentRoute");
 const customizationSettingsRoute = require("./Routes/customizationSettingsRoute");
 const automatedWorkflowsRoutes = require("./Routes/automatedWorkflowsRoute");
+// const liveChatRoute = require("./Routes/liveChatRoute");
 
+const http = require('http');
+const socketIO = require('socket.io');
+const ChatController = require('./Controller/ChatController'); // Import ChatController
 
+// Set up a basic route to serve an HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Create an HTTP server and attach Socket.IO to it
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Initialize ChatController with the Socket.IO instance
+const chatController = new ChatController(io);
+
+// Handle WebSocket connections using ChatController
+io.on('connection', (socket) => {
+  chatController.onConnection(socket);
+});
+
+// Middleware
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // MongoDB Connection
 const mongoURI = "mongodb://127.0.0.1:27017/SE-Project";
-mongoose
-  .connect(mongoURI)
+mongoose.connect(mongoURI)
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
-app.use(express.json());
+  .catch(err => console.error("Could not connect to MongoDB...", err));
 
+
+// Define your routes
 app.use("/api/v1", auth);
-
 app.use(authenticationMiddleware);
-
 app.use("/api/customizationSettings", customizationSettingsRoute);
-
 app.use("/api/chatMessages", chatMessagesRoutes);
-
 app.use("/api/security-settings", securitySettingsRoutes);
-
 app.use("/api/knowledgeBase", knowledgeBaseRoutes);
-
 app.use("/api/reports", reportsAndAnalyticsRoutes);
-
 app.use("/api/support-agents", supportAgentRoutes);
-
 app.use("/api/tickets", ticketsRoute);
-
 app.use("/api/users", userRoutes);
+app.use("/api/automatedWorkflows", automatedWorkflowsRoutes);
+// app.use("/api/liveChat", liveChatRoute);
 
-app.use("/api/automatedWorkflows",automatedWorkflowsRoutes)
-
+// Set the port for the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+// Start the server
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
