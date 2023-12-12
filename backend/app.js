d6201975c1722fd0bc99f0f718431f31bcc0d2a3
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-12 15:55:35 +0200

    done merge

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,13 @@
 // Import required packages and modules
 require("dotenv").config();
-
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const http = require("http");
 
-// Import routes and middleware
-const authRoutes = require("./Routes/auth");
-const authenticationMiddleware = require("./Middleware/authentication");
-const userRoutes = require("./Routes/usersRoute");
-const ticketsRoute = require("./Routes/ticketsRoute");
-const securitySettingsRoutes = require("./Routes/securitySettingsRoute");
-const knowledgeBaseRoutes = require("./Routes/knowledgeBaseRoute");
-const reportsAndAnalyticsRoutes = require("./Routes/reportsAndAnalyticsRoute");
-const supportAgentRoutes = require("./Routes/supportAgentRoute");
-const customizationSettingsRoute = require("./Routes/customizationSettingsRoute");
-const automatedWorkflowsRoutes = require("./Routes/automatedWorkflowsRoute");
-const chatRoute = require("./Routes/chatRoute");
-const messageRoute = require("./Routes/messageRoute");
-const emailSystemRoutes = require("./Routes/emailSystemRoute");
-
-
-// Initialize Express app and HTTP server
-
+// Initialize Express app
+const app = express();
 
 // Set CORS options
 const corsOptions = {
@@ -42,41 +24,46 @@ app.use(express.urlencoded({ extended: false }));
 // MongoDB Connection
 const mongoURI = "mongodb://127.0.0.1:27017/SE-Project";
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongoURI)
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+
+// Import routes and middleware
+const authRoutes = require("./Routes/auth");
+const authenticationMiddleware = require("./Middleware/authentication");
+const userRoutes = require("./Routes/usersRoute");
+const ticketsRoute = require("./Routes/ticketsRoute");
+const securitySettingsRoutes = require("./Routes/securitySettingsRoute");
+const knowledgeBaseRoutes = require("./Routes/knowledgeBaseRoute");
+const reportsAndAnalyticsRoutes = require("./Routes/reportsAndAnalyticsRoute");
+const supportAgentRoutes = require("./Routes/supportAgentRoute");
+const customizationSettingsRoute = require("./Routes/customizationSettingsRoute");
+const automatedWorkflowsRoutes = require("./Routes/automatedWorkflowsRoute");
+const chatRoute = require("./Routes/chatRoute");
+const messageRoute = require("./Routes/messageRoute");
+const emailSystemRoutes = require("./Routes/emailSystemRoute");
 
 // Public routes
 app.use("/api/v1", authRoutes); // Auth routes (login, register, etc.)
 
 // Protected routes with authentication middleware
-app.use(
-  "/api/customizationSettings",
-  authenticationMiddleware,
-  customizationSettingsRoute
-);
+app.use("/api/customizationSettings", authenticationMiddleware, customizationSettingsRoute);
 app.use("/api/emails", authenticationMiddleware, emailSystemRoutes);
-app.use(
-  "/api/security-settings",
-  authenticationMiddleware,
-  securitySettingsRoutes
-);
+app.use("/api/security-settings", authenticationMiddleware, securitySettingsRoutes);
 app.use("/api/knowledgeBase", authenticationMiddleware, knowledgeBaseRoutes);
 app.use("/api/reports", authenticationMiddleware, reportsAndAnalyticsRoutes);
 app.use("/api/support-agents", authenticationMiddleware, supportAgentRoutes);
 app.use("/api/tickets", authenticationMiddleware, ticketsRoute);
-app.use("/api/users",   authenticationMiddleware,
-userRoutes);
-app.use(
-  "/api/automatedWorkflows",
-  authenticationMiddleware,
-  automatedWorkflowsRoutes
-);
+app.use("/api/users", authenticationMiddleware, userRoutes);
+app.use("/api/automatedWorkflows", authenticationMiddleware, automatedWorkflowsRoutes);
 
 // Chat and message routes (assuming these need authentication)
 app.use("/api/chat", authenticationMiddleware, chatRoute);
 app.use("/api/message", authenticationMiddleware, messageRoute);
 
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
 
 // Set the port for the server
 const port = process.env.PORT || 3000;
