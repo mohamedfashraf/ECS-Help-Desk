ab9b297b868fb5bef46b041a929110b901d89b06
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-13 22:19:48 +0200

    trying to link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Login from "./pages/Login2";
 import NavBar from "./components/NavBar";
 import { AuthContext } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={user ? <Chat /> : <Login />} />
           <Route path="/register" element={user ? <Chat /> : <Register />} />
-          <Route path="/login" element={user ? <Chat /> : <Login />} />
+          <Route path="/login2" element={user ? <Chat /> : <Login />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Container>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        linearButton: "linear-gradient(to right, #2E4CEE, #221EBF, #040F75)",
+        customblack: "#0F0F0F", // other colors...
+      },
+      backgroundImage: {
+        linearButton: "linear-gradient(to right, #2E4CEE, #221EBF, #040F75)",
+        // other background images...
+      },
+      height: {
+        "screen-150": "13px", // This means 150% of the viewport height
+      },
+      keyframes: {
+        fadeDown: {
+          "0%": { opacity: "0", transform: "translateY(-10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    animation: {
+      "fade-down": "fadeDown 1s ease-out",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
