632b1c360a2aea5e8b86f61d6d06d7c066769927
Yassa122 <yassa.ashraf@yahoo.com>
2023-12-14 17:11:58 +0200

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
-import Login from "./pages/Login2";
 import NavBar from "./components/NavBar";
 import { AuthContext } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
