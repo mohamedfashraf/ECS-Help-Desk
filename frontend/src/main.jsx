42e4f14e59384c86147597a82d069d530dd99a02
Yassa122 <yassa.ashraf@yahoo.com>
2023-12-16 00:25:03 +0200

    modified the main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ThemeProvider from './utils/ThemeContext';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import ThemeProvider from "./utils/ThemeContext"; // Imported ThemeProvider
+import App from "./App";
+import "./index2.css"; // Assuming you still need these CSS imports
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthContextProvider } from "./context/AuthContext"; // Assuming you still need the AuthContextProvider
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
-        <App />
+        {" "}
+        {/* Added ThemeProvider here */}
+        <AuthContextProvider>
+          {" "}
+          {/* Keep existing AuthContextProvider */}
+          <App />
+        </AuthContextProvider>
       </ThemeProvider>
     </Router>
   </React.StrictMode>
