6a01a15d81ef679343ccf5ec2c17306d2ba07cf4
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-17 02:17:34 +0200

    added chat messages and working

--- frontend/src/components/chat/ChatBox.jsx
+++ frontend/src/components/chat/ChatBox.jsx
@@ -1,10 +1,7 @@
-// ChatBox.jsx
-
 import { useContext } from "react";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
 import { AuthContext } from "../../context/AuthContext";
-import { Stack } from "react-bootstrap";
 import moment from "moment";
 import InputEmoji from "react-input-emoji";
 import { useState } from "react";
@@ -20,83 +17,77 @@ const ChatBox = () => {
 
   const [textMessage, setTextMessage] = useState("");
 
-  console.log("textMessage", textMessage);
-
   if (!recipientUser) {
-    console.log("No recipientUser");
     return (
-      <p style={{ textAlign: "center", width: "100%" }}>
-        No Conversation selected yet...
-      </p>
+      <p className="text-center w-full">No Conversation selected yet...</p>
     );
   }
 
   if (isMessagesLoading) {
-    return (
-      <p style={{ textAlign: "center", width: "100%" }}>Loading chat...</p>
-    );
+    return <p className="text-center w-full">Loading chat...</p>;
   }
 
   return (
-    <>
-      <Stack gap={4} className="chat-box">
+    <div className="flex flex-col flex-grow p-4 chat-box h-full">
+      <div className="flex-shrink-0 mb-6">
         <div className="chat-header">
-          <strong>{recipientUser?.name}</strong>
+          <strong className="text-lg">{recipientUser?.name}</strong>
         </div>
-        <Stack gap={3} className="messages">
-          {messages.map((message, index) => (
-            <Stack
-              key={index}
+      </div>
+      <div className="flex-1 overflow-y-auto messages mb-10">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`${
+              message?.senderId === user?._id
+                ? "self-message flex justify-end mb-4"
+                : "other-message flex justify-start mb-4"
+            }`}
+          >
+            <div
               className={`${
                 message?.senderId === user?._id
-                  ? "message self align-self-end flex-grow-0"
-                  : "message align-self-start flex-grow-0"
-              }`}
+                  ? "self-message-box bg-green-500 text-white"
+                  : "other-message-box bg-gray-300 text-black"
+              } p-2 rounded`}
             >
-              <span>{message.text}</span>
-              <span className="message-footer">
+              <span className="text-md break-words">{message.text}</span>
+              <span className="message-footer text-sm text-gray-500">
                 {moment(message.createdAt).calendar()}
               </span>
-            </Stack>
-          ))}
-        </Stack>
-        <Stack
-          direction="horizontal"
-          gap={3}
-          className="chat-input flex-grow-0"
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="flex space-x-3 chat-input">
+        <InputEmoji
+          value={textMessage}
+          onChange={setTextMessage}
+          fontFamily="nunito"
+          borderColor="rgba(72,112,223,0.2)"
+          className="flex-1"
+        />
+        <button
+          className="send-btn bg-blue-500 text-white h-12 w-12 rounded-full flex items-center justify-center hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+          onClick={async () =>
+            sendTextMessage(textMessage, user, currentChat._id, sendTextMessage)
+          }
         >
-          <InputEmoji
-            value={textMessage}
-            onChange={setTextMessage}
-            fontFamily="nunito"
-            borderColor="rgba(72,112,223,0.2)"
-          />
-          <button
-            className="send-btn"
-            onClick={async () =>
-              sendTextMessage(
-                textMessage,
-                user,
-                currentChat._id,
-                sendTextMessage
-              )
-            }
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-send-fill"
+            viewBox="0 0 16 16"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-send-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
-            </svg>
-          </button>
-        </Stack>
-      </Stack>
-    </>
+            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+          </svg>
+        </button>
+      </div>
+    </div>
   );
 };
 
 export default ChatBox;
+w
