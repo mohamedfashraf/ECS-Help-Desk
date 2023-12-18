3c3ecb9c0e280efa429f62b97488b50f4edd451a
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-18 19:06:11 +0200

    d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,12 @@ import Chats from "./pages/Chat";
 import Register from "./pages/SignUp";
 import Tickets from "./pages/Tickets";
 
+import "./css/style.css";
 
+import "./charts/ChartjsConfig";
+import "./css/style.css";
 
+import "./charts/ChartjsConfig";
 import "./css/style.css";
 
 import "./charts/ChartjsConfig";
@@ -25,29 +29,29 @@ function App() {
     document.querySelector("html").style.scrollBehavior = "auto";
     window.scroll({ top: 0 });
     document.querySelector("html").style.scrollBehavior = "";
-
-    
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <ChatContextProvider user={user}>
         <Routes>
-          <Route exact path="/login" element={<Login />} />
+          <Route
+            exact
+            path="/login"
+            element={user ? <Dashboard /> : <Login />}
+          />
           <Route exact path="/" element={user ? <Dashboard /> : <Login />} />
-          {<Route exact path="*" element={<Navigate to="/" />} />}
           <Route exact path="*" element={<Navigate to="/" />} />
           <Route path="/chats" element={user ? <Chats /> : <Login />} />
-          <Route path="/tickets" element={user ? <Tickets /> : <Login />} />
-
           <Route
             path="/register"
             element={user ? <Dashboard /> : <Register />}
           />
+          <Route path="/tickets" element={user ? <Tickets /> : <Login />} />
         </Routes>
       </ChatContextProvider>
     </>
   );
 }
 
-export default App;
+export default App;
