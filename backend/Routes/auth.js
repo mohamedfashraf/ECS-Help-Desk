d5e2730d47866ade48c175c527a08accbf10ff88
Yassa122 <yassa.ashraf@yahoo.com>
2023-12-18 17:32:04 +0200

    updates on google login

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,46 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controller/userController");
-const authorizationMiddleware = require("../Middleware/authorization");
 const passport = require("passport");
 
-// * login
+// login
 router.post("/login", userController.login);
 
-// * register
+// register
 router.post("/register", userController.register);
 
-router.get("/login/success", (req, res) => {
-  if (req.user) {
-    res.status(200).json({
-      error: false,
-      message: "Successfully Loged In",
-      user: req.user,
-    });
-  } else {
-    res.status(403).json({ error: true, message: "Not Authorized" });
-  }
-});
-
-router.get("/login/failed", (req, res) => {
-  res.status(401).json({
-    error: true,
-    message: "Log in failure",
-  });
-});
-
-router.get("/google", passport.authenticate("google", ["profile", "email"]));
+// Google OAuth routes
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 router.get(
   "/google/callback",
   passport.authenticate("google", {
-    successRedirect: "http://localhost:3000/auth/google/callback/",
+    successRedirect: "http://localhost:3000/dashboard", // Redirect to your frontend dashboard
     failureRedirect: "/login/failed",
   })
 );
 
+// Logout route
 router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect("http://localhost:3000/auth/google/callback/");
+  res.redirect("http://localhost:3000/login"); // Redirect to your frontend login page
 });
-module.exports = router; // ! Don't forget to export the router
+
+module.exports = router;
