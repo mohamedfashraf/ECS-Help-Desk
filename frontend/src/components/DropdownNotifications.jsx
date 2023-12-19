4fa944b9a277e43624030a0b9030369f555d5174
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-19 13:59:24 +0200

    update dime show up and notifications

--- frontend/src/components/DropdownNotifications.jsx
+++ frontend/src/components/DropdownNotifications.jsx
@@ -8,7 +8,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DropdownNotifications = ({ align }) => {
   const [messages, setMessages] = useState([]);
-  const [showAllMessages, setShowAllMessages] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const { user } = useContext(AuthContext);
@@ -41,10 +40,6 @@ const DropdownNotifications = ({ align }) => {
     fetchMessages();
   }, [dropdownOpen]);
 
-  const handleClickShowMore = () => {
-    setShowAllMessages(!showAllMessages);
-  };
-
   const handleClickIcon = () => {
     setDropdownOpen((prev) => !prev); // Toggle the dropdown state
   };
@@ -53,6 +48,10 @@ const DropdownNotifications = ({ align }) => {
     if (dropdown.current && !dropdown.current.contains(event.target)) {
       setDropdownOpen(false); // Close the dropdown if clicked outside
     }
+  };
+
+  const handleCloseDropdown = () => {
+    setDropdownOpen(false);
   };
 
   useEffect(() => {
@@ -77,50 +76,50 @@ const DropdownNotifications = ({ align }) => {
       </button>
 
       <Transition
-        className={`origin-top-right z-10 absolute top-full -mr-48 sm:mr-0 min-w-80 bg-white dark:bg-slate-800 border border-slate-200 dark:border-slate-700 py-1.5 rounded shadow-lg overflow-hidden mt-1 ${
+        className={`origin-top-right z-10 absolute top-full -mr-48 sm:mr-0 min-w-80 max-h-60 overflow-y-auto bg-white dark:bg-slate-800 border border-slate-200 dark:border-slate-700 py-1.5 rounded shadow-lg overflow-hidden mt-1 ${
           align === "right" ? "right-0" : "left-0"
         }`}
         show={dropdownOpen}
       >
         <div ref={dropdown}>
+          <div className="flex justify-end pr-2">
+            <button
+              className="text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-600"
+              onClick={handleCloseDropdown}
+            >
+              <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 6.293a1 1 0 011.414 0L10 8.586l3.293-3.293a1 1 0 111.414 1.414L11.414 10l3.293 3.293a1 1 0 01-1.414 1.414L10 11.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586 10 5.293 6.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
           <div className="text-xs font-semibold text-slate-400 dark:text-slate-500 uppercase pt-1.5 pb-2 px-4">
             Notifications
           </div>
           <ul>
-            {messages
-              .slice(0, showAllMessages ? messages.length : 5)
-              .map((message, index) => (
-                <li
-                  key={index}
-                  className={`border-b border-slate-200 dark:border-slate-700 ${
-                    index === messages.length - 1 ? "last:border-0" : ""
-                  }`}
-                >
-                  <Link
-                    className="block py-2 px-4 hover:bg-slate-50 dark:hover:bg-slate-700/20"
-                    to="#0"
-                    onClick={() => setDropdownOpen(false)}
-                  >
-                    <span className="block text-sm mb-2">
-                      {message.message}
-                    </span>
-                    <span className="block text-xs font-medium text-slate-400 dark:text-slate-500">
-                      {new Date(message.timestamp).toLocaleString()}
-                    </span>
-                  </Link>
-                </li>
-              ))}
-          </ul>
-          {messages.length > 5 && (
-            <div className="px-4 pb-2 text-center">
-              <button
-                className="text-sm text-slate-500 hover:text-slate-700 dark:text-slate-400 dark:hover:text-slate-600"
-                onClick={handleClickShowMore}
+            {messages.map((message, index) => (
+              <li
+                key={index}
+                className={`border-b border-slate-200 dark:border-slate-700 ${
+                  index === messages.length - 1 ? "last:border-0" : ""
+                }`}
               >
-                {showAllMessages ? "Show Less" : "Show More"}
-              </button>
-            </div>
-          )}
+                <Link
+                  className="block py-2 px-4 hover:bg-slate-50 dark:hover:bg-slate-700/20"
+                  to="#0"
+                  onClick={handleCloseDropdown}
+                >
+                  <span className="block text-sm mb-2">{message.message}</span>
+                  <span className="block text-xs font-medium text-slate-400 dark:text-slate-500">
+                    {new Date(message.timestamp).toLocaleString()}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </Transition>
     </div>
