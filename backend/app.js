baca91a9e4d06be50fba32c84622d45f6264a46d
MohameddAshraff89 <mohameddashraff69@gmail.com>
2023-12-23 20:58:25 +0200

    added backup

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,12 @@
 // Import required packages and modules
 require("dotenv").config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const http = require("http");
-
-//google auth2
+const { exec } = require("child_process");
+const cron = require("node-cron");
 
 // Import routes and middleware
 const authRoutes = require("./Routes/auth");
@@ -25,8 +24,9 @@ const messageRoute = require("./Routes/messageRoute");
 const emailSystemRoutes = require("./Routes/emailSystemRoute");
 const queuesRoutes = require("./Routes/queuesRoute");
 
-
 // Initialize Express app and HTTP server
+const app = express();
+
 //google auth2
 const passport = require("passport");
 const cookieSession = require("cookie-session");
@@ -38,8 +38,8 @@ app.use(
   session({
     secret: "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", // Replace with your own secret
     resave: false,
-    saveUninitialized: false, // Change to true if you want to store sessions for unauthenticated users
-    cookie: { secure: process.env.NODE_ENV === "production" }, // Secure cookies in production
+    saveUninitialized: false,
+    cookie: { secure: process.env.NODE_ENV === "production" },
   })
 );
 
@@ -56,8 +56,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(cors());
-
-// Middleware setup
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -65,14 +63,43 @@ app.use(express.urlencoded({ extended: false }));
 // MongoDB Connection
 const mongoURI = "mongodb://127.0.0.1:27017/SE-Project";
 mongoose
-  .connect(mongoURI)
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-app.use("/api/v1", authRoutes); // Auth routes (login, register, etc.)
+// Auth routes (login, register, etc.)
+app.use("/api/v1", authRoutes);
 
-app.use("/auth", authRoutes);
-// Public routes
+// Backup MongoDB Route
+app.get("/api/backup", (req, res) => {
+  const backupFolder = "C:\\Users\\moham\\OneDrive\\Desktop\\backups"; // Set the backup folder path
+  const timestamp = new Date().toISOString().replace(/[-:]/g, "");
+
+  const mongodumpCommand = `"C:\\Program Files\\MongoDB\\Tools\\100\\bin\\mongodump" --uri=${mongoURI} --out=${backupFolder}/${timestamp} --db=SE-Project`;
+
+  exec(mongodumpCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error during backup: ${error.message}`);
+      return res.status(500).json({ error: "Backup failed" });
+    }
+
+    console.log(`Backup successful: ${stdout}`);
+    return res.status(200).json({ message: "Backup successful" });
+  });
+});
+
+
+// Schedule backup using cron job (every day at midnight)
+cron.schedule("0 0 * * *", () => {
+  // Trigger the backup route
+  exec("curl http://localhost:3000/api/backup", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error triggering backup: ${error.message}`);
+    } else {
+      console.log(`Backup triggered: ${stdout}`);
+    }
+  });
+});
 
 // Protected routes with authentication middleware
 app.use(
@@ -92,9 +119,12 @@ app.use("/api/support-agents", authenticationMiddleware, supportAgentRoutes);
 app.use("/api/tickets", authenticationMiddleware, ticketsRoute);
 app.use("/api/users", authenticationMiddleware, userRoutes);
 
-app.use("/api/automatedWorkflows", authenticationMiddleware, automatedWorkflowsRoutes);
+app.use(
+  "/api/automatedWorkflows",
+  authenticationMiddleware,
+  automatedWorkflowsRoutes
+);
 app.use("/api/queues", authenticationMiddleware, queuesRoutes);
-
 
 // Chat and message routes (assuming these need authentication)
 app.use("/api/chat", authenticationMiddleware, chatRoute);
@@ -108,5 +138,5 @@ const port = process.env.PORT || 3000;
 
 // Start the server
 server.listen(port, () => {
-  console.log(`Server running on port ${port} `);
+  console.log(`Server running on port ${port}`);
 });
